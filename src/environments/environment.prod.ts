0d21f5eedf235465387589e7c419d64049ff6a55
Adrian Solano <adsolanoram@gmail.com>
2026-02-05 15:17:38 -0600

    SSL test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     // baseUrl: 'https://api.papeleria.alumbrado.net/api',
-    baseUrl: 'http://66.175.239.69:8081/api',
+    baseUrl: 'https://66.175.239.69:8081/api',
     logoGobiernoUrl: './assets/img/logo-gobierno.jpeg'
 };
