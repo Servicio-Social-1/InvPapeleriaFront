cdc1d321f7347159f11faf9ed8702f6ea58b5783
Adrian Solano <adsolanoram@gmail.com>
2026-02-05 15:30:14 -0600

    SSL Test III

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     // baseUrl: 'https://api.papeleria.alumbrado.net/api',
-    baseUrl: 'https://papeleriadap.sobse.com.mx/api/',
+    baseUrl: 'https://papeleriadap.sobse.com.mx/api',
     logoGobiernoUrl: './assets/img/logo-gobierno.jpeg'
 };
